cc0329df8491f8e9bb7ce01072652da1a0457696
ArturGG <arturad20@gmail.com>
2024-04-10 16:09:08 +0300

    Add Functions and defines

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,4 +1,55 @@
-﻿int main() {
+﻿#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <cmath>
+#include <stdexcept>
+
+class PumpCalculator {
+public:
+    PumpCalculator(double efficiency, double dailyConsumption, double operationTime, double geodeticHeight,
+        double staticHead, double darcyCoefficient, double pipeLength, double pipeDiameter)
+        : k(efficiency), V(dailyConsumption), t(operationTime), H_g(geodeticHeight),
+        H_s(staticHead), D(darcyCoefficient), L(pipeLength), d(pipeDiameter) {
+        if (k <= 0 || t <= 0 || d <= 0) {
+            throw std::invalid_argument("Efficiency, operation time, and pipe diameter must be greater than 0.");
+        }
+    }
+
+    double calculatePumpPower() {
+        auto Q = calculateFlowRate();
+        auto velocity = calculateVelocity(Q);
+        auto h_l = calculateFrictionHeadLoss(velocity);
+        auto h_m = calculateLocalHeadLoss(h_l);
+        auto H = calculateTotalHead(h_l, h_m);
+
+        return (9.81 * Q * H) / k;
+    }
+
+private:
+    double k, V, t, H_g, H_s, D, L, d;
+
+    double calculateFlowRate() const {
+        return V / (3600 * t);
+    }
+
+    double calculateVelocity(double Q) const {
+        return (4 * Q) / (M_PI * std::pow(d, 2));
+    }
+
+    double calculateFrictionHeadLoss(double velocity) const {
+        return D * (L / d) * std::pow(velocity, 2) / 19.6;
+    }
+
+    double calculateLocalHeadLoss(double h_l) const {
+        return 0.1 * h_l;
+    }
+
+    double calculateTotalHead(double h_l, double h_m) const {
+        return H_g + h_l + h_m + H_s;
+    }
+};
+
+int main() {
     try {
         double k, V, t, H_g, H_s, D, L, d;
         std::cout << "Enter the pump's efficiency (k): ";
